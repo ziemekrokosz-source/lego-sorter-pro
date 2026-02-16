4ae6ed3a14a4959d560af85a5d454c0059061ae8
ziemekrokosz-source <ziemekrokosz@gmail.com>
2026-02-16 10:53:50 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import { Layout } from './components/Layout';
+import { Layout } from './Layout';
 import { SetSearch } from './components/SetSearch';
 import { SetCard } from './components/SetCard';
 import { SetDetail } from './components/SetDetail';
